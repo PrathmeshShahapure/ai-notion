2cb309fc8e6b10a70079c1ed1019fd80120f654f
PrathmeshShahapure <prathmeshshahapure28@gmail.com>
2026-07-10 15:59:04 +0530

    chore : initial setup and clerk auth

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,10 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import {
+  ClerkProvider,
+} from "@clerk/nextjs";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -27,7 +31,13 @@ export default function RootLayout({
       lang="en"
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
-      <body className="min-h-full flex flex-col">{children}</body>
+      <body className="min-h-full flex flex-col font-mono max-w-7xl mx-auto">
+        <ClerkProvider>
+          <Header />
+
+          {children}
+        </ClerkProvider>
+      </body>
     </html>
   );
 }
---
+++ components/Header.tsx
@@ -0,0 +1,29 @@
+"use client";
+import React from "react";
+import { useUser } from "@clerk/nextjs";
+import { Show, SignInButton, SignUpButton, UserButton } from "@clerk/nextjs";
+const Header = () => {
+  const { user } = useUser();
+  return (
+    <div className="flex justify-between items-center p-4 gap-4 h-16">
+      {" "}
+      <div>{user && <h1>{user?.firstName}`s Space </h1>}</div>
+      <div></div>
+      <div className="flex gap-4">
+        <Show when="signed-out">
+          <SignInButton />
+          <SignUpButton>
+            <button className="bg-[#6c47ff] text-white rounded-full font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 cursor-pointer">
+              Sign Up
+            </button>
+          </SignUpButton>
+        </Show>
+        <Show when="signed-in">
+          <UserButton />
+        </Show>
+      </div>
+    </div>
+  );
+};
+
+export default Header;
